95902961206705d0b6bcaa88b30570812ac46bd9
jdidat <jdidat@purdue.edu>
2018-07-07 17:57:45 -0400

    finsihed get request processing

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,4 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/')
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /products'
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+module.exports = router;
